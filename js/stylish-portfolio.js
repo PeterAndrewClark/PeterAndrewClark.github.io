74d94229cb52016712c6e543e0b9e20853f3c11b
PeterClark <clarkpeterandrew@gmail.com>
2020-03-19 14:35:04 -0400

    Update to everything above the fold
    Excluding the nav

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -62,3 +62,85 @@ var onMapClickHandler = function(event) {
 }
 // Enable map zooming with mouse scroll when the user clicks the map
 $('.map').on('click', onMapClickHandler);
+
+
+//Custom modifications for text animation
+var words = (function(){
+  var words = [
+      'Performance',
+      'Growth',
+      'Brand',
+      'Content',
+      'Social Media',
+      'Traditional',
+      'Data-driven',
+      'Automated'
+      ],
+    el = document.querySelector('.verb'),
+    currentIndex,
+    currentWord,
+    prevWord,
+    duration = 1000;
+
+  var _getIndex = function(max, min){
+    currentIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    //Generates a random number between beginning and end of words array
+    return currentIndex;
+  };
+
+  var _getWord = function(index){
+    currentWord = words[index];
+
+    return currentWord;
+  };
+
+  var _clear = function() {
+
+    setTimeout(function(){
+      el.className = 'verb';
+    }, duration/4);
+  };
+
+  var _toggleWord = function(duration){
+    setInterval(function(){
+      //Stores value of previous word
+      prevWord = currentWord;
+
+      //Generate new current word
+      currentWord = words[_getIndex(words.length-1, 0)];
+
+      //Generate new word if prev matches current
+      if(prevWord === currentWord){
+
+        currentWord = words[_getIndex(words.length-1, 0)];
+      }
+
+      //Swap new value
+      el.innerHTML = currentWord;
+
+      //Clear class styles
+      _clear();
+
+      //Fade in word
+      el.classList.add(
+        'js-block',
+        'js-fade-in-verb'
+        );
+
+    }, duration);
+  };
+
+  var _init = function(){
+    _toggleWord(duration);
+  };
+
+  //Public API
+  return {
+    init : function(){
+      _init();
+    }
+  };
+})();
+
+words.init();
